43085c7db6f38dadce7ac538fb9c817480485a4e
vaishnavi90175 <2400090175@kluniversity.in>
2026-01-08 14:57:12 +0530

    commited

---
+++ src/main/java/com/klu/DiConfigurationXmlApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiConfigurationXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiConfigurationXmlApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/klu/model/CourseRegistration.java
@@ -0,0 +1,28 @@
+package com.klu.model;
+
+public class CourseRegistration {
+ private int rollNo;
+ private String studentName;
+ private String courseName;
+ private int semester;
+ public CourseRegistration(int rollNo, String studentName) {
+  this.rollNo= rollNo;
+  this.studentName= studentName;
+  
+ }
+ public void setCourseName(String courseName) {
+  this.courseName= courseName;
+  
+ }
+ public void setSemester(int semester) {
+  this.semester = semester;
+ }
+ public void display() {
+  System.out.println("RollNo:"+rollNo);
+  System.out.println("Name:"+studentName);
+  System.out.println("Course:"+courseName);
+  System.out.println("Semester:"+semester);
+  
+ }
+
+}
